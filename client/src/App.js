eed75e2c191cd6f1b9c3b1c7d22c0d8fc65192ff
viveksingh-01 <viveksingh9301@gmail.com>
2020-12-02 19:09:27 +0530

    Add custom bootstrap file from Bootswatch

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const App = () => {
-  return <div>Welcome to ShopEasy!</div>;
+  return (
+    <div>
+      <h1>Welcome to ShopEasy!</h1>
+    </div>
+  );
 };
 
 export default App;
