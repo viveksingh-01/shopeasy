ead926a35d6659a471ac361a9023e5e9150eb216
viveksingh-01 <viveksingh9301@gmail.com>
2020-12-03 12:56:43 +0530

    Implement routing in Home screen and navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import HomeScreen from './screens/HomeScreen';
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className="my-3">
         <Container>
-          <HomeScreen />
+          <Route path="/" component={HomeScreen} exact />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
