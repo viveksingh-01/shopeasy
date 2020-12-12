7fff6ee7824d71280dc9026d063a5d3bfbc25c0e
viveksingh-01 <viveksingh9301@gmail.com>
2020-12-12 23:38:54 +0530

    Add products routes

---
+++ server/routes/products-route.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import Product from '../models/Product.js';
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  try {
+    const products = await Product.find();
+    if (!products) {
+      return res.status(404).json({ message: 'No products found.' });
+    }
+    res.status(200).json(products);
+  } catch (error) {
+    res.json({ message: error.message });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const product = await Product.findOne({ _id: req.params.id });
+    if (!product) {
+      return res.status(404).json({ message: 'Product not found.' });
+    }
+    res.status(200).json(product);
+  } catch (error) {
+    res.json({ message: error.message });
+  }
+});
+
+export default router;
