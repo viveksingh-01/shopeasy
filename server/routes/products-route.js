4e30860033cd57291412b5250d9cbcdae1dc50d0
viveksingh-01 <viveksingh9301@gmail.com>
2020-12-13 00:43:35 +0530

    Update 'products-route.js'

--- server/routes/products-route.js
+++ server/routes/products-route.js
@@ -6,9 +6,6 @@ const router = express.Router();
 router.get('/', async (req, res) => {
   try {
     const products = await Product.find();
-    if (!products) {
-      return res.status(404).json({ message: 'No products found.' });
-    }
     res.status(200).json(products);
   } catch (error) {
     res.json({ message: error.message });
