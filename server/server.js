178e734737d4e14d90f65e177376af53c36c6b34
viveksingh-01 <viveksingh9301@gmail.com>
2020-12-12 23:40:43 +0530

    Implement routing for products

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import colors from 'colors';
 import connectToDB from './config/db.js';
-import products from './data/products.js';
+import productsRoute from './routes/products-route.js';
 
 const app = express();
 
@@ -14,14 +14,7 @@ app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
-app.get('/api/products', (req, res) => {
-  res.status(200).json(products);
-});
-
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find(({ _id }) => _id === req.params.id);
-  res.status(200).json(product);
-});
+app.use('/api/products', productsRoute);
 
 const PORT = process.env.PORT || 5000;
 
