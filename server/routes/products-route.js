eb403057829ce9e7ec8cf6943d3c41be83964b92
viveksingh-01 <viveksingh9301@gmail.com>
2020-12-13 09:24:32 +0530

    Add custom error-handler and pass unknown errors from products-route to it

--- server/routes/products-route.js
+++ server/routes/products-route.js
@@ -3,16 +3,16 @@ import Product from '../models/Product.js';
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
+router.get('/', async (req, res, next) => {
   try {
     const products = await Product.find();
     res.status(200).json(products);
   } catch (error) {
-    res.json({ message: error.message });
+    return next(error);
   }
 });
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', async (req, res, next) => {
   try {
     const product = await Product.findOne({ _id: req.params.id });
     if (!product) {
@@ -20,7 +20,7 @@ router.get('/:id', async (req, res) => {
     }
     res.status(200).json(product);
   } catch (error) {
-    res.json({ message: error.message });
+    return next(error);
   }
 });
 
--- server/server.js
+++ server/server.js
@@ -16,6 +16,14 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productsRoute);
 
+app.use('/', (error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code ?? 500);
+  res.json({ message: error.message ?? 'An unknown error occurred.' });
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
